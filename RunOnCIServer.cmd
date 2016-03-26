f0f89463346ced3061bb598e3330d4baaf144374
John Howard <jhoward@microsoft.com>
2016-03-25 20:11:31 -0700

    Tidy up for TP5 servers
    Signed-off-by: John Howard <jhoward@microsoft.com>

---
+++ RunOnCIServer.cmd
@@ -0,0 +1,10 @@
+REM Wrapper script to perform a local run of CI assuming everything is already setup.
+REM This is set for the CI servers. The TP5+ servers should be pre-configured with these
+REM variables already - the C drive is the system drive, D is a fast SSD.
+
+set SOURCES_DRIVE=c
+set SOURCES_SUBDIR=gopath
+set TESTRUN_DRIVE=d
+set TESTRUN_SUBDIR=CI
+
+sh ./executeCI.sh
--- nssmdocker.W2WCIServers.cmd
+++ nssmdocker.W2WCIServers.cmd
@@ -1,17 +1,11 @@
 @echo off
 
-rem This is a varient of runDockerDaemon.cmd in programdata\docker installed by Install-ContainerHost.ps1
-rem It has some key differences for use in WIndows CI.
+rem This is a variant of runDockerDaemon.cmd in programdata\docker installed by Install-ContainerHost.ps1
+rem It has some key differences for use in Windows CI for TP5+
 rem - Daemon is NOT debug
 rem - Daemon is renamed to dockernssm.exe to spot easily in task manager
 rem - Daemon is redirected to d:\daemon, D: being a fast SSD on CI machines
 rem - TEMP and TMP are redirected to d:\temp (SSD drive)
-
-
-set certs=%ProgramData%\docker\certs.d
-
-rem if exist %ProgramData%\docker (goto :run)
-rem mkdir %ProgramData%\docker
 
 set TEMP=d:\temp
 set TMP=d:\temp
@@ -21,13 +15,5 @@ if exist d:\daemon (goto :run)
 mkdir d:\daemon
 
 :run
-rem if exist %certs%\server-cert.pem (if exist %ProgramData%\docker\tag.txt (goto :secure))
-rem docker daemon -D
- 
 copy %systemroot%\system32\docker.exe %systemroot%\system32\dockernssm.exe /Y
 dockernssm daemon --graph=d:\daemon --pidfile=d:\daemon\daemon.pid
-goto :eof
-
-rem :secure
-REM This isn't used for CI machines.
-rem docker daemon -D -H 0.0.0.0:2376 --tlsverify --tlscacert=%certs%\ca.pem --tlscert=%certs%\server-cert.pem --tlskey=%certs%\server-key.pem
