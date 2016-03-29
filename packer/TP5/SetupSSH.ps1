4a5626ae0dea4882aca0f3d1519881e25d2e769d
John Howard <jhoward@microsoft.com>
2016-03-29 13:58:50 -0700

    Now really with SSH
    Signed-off-by: John Howard <jhoward@microsoft.com>

---
+++ packer/TP5/SetupSSH.ps1
@@ -0,0 +1,23 @@
+#-----------------------
+# SetupSSH.ps1
+#-----------------------
+
+Write-Host "INFO: Executing SetupSSH.ps1"
+
+# Open the firewall
+Start-Process -wait netsh -ArgumentList "advfirewall firewall add rule name=SSH dir=in action=allow protocol=TCP localport=22"
+c:\cygwin\bin\bash -c 'export PATH=/usr/sbin:/usr/bin:$PATH;/usr/bin/ssh-host-config -y -w password -c \"binmode ntsec\";\
+echo \"PasswordAuthentication no\" >> /etc/sshd_config;\
+mkdir -p ~/.ssh;\
+chmod 700 ~/.ssh;\
+cp /cygdrive/c/packer/authorized_keys ~/.ssh/authorized_keys;\
+chmod 644 ~/.ssh/authorized_keys;\
+/usr/bin/cygrunsrv -S sshd' 2>&1 | out-null
+
+
+
+### TODO ####
+#/usr/bin/ssh-host-config -y -w {{ user `password` }}' -c "binmode ntsec";echo hello'
+
+Write-Host "INFO: SetupSSH.ps1 completed"
+
