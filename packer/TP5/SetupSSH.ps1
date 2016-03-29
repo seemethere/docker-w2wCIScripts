ea17a7ba6a396255f6f2f2d7047b94b6dc41a35b
John Howard <jhoward@microsoft.com>
2016-03-29 14:00:11 -0700

    SSH user password
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- packer/TP5/SetupSSH.ps1
+++ packer/TP5/SetupSSH.ps1
@@ -6,7 +6,7 @@ Write-Host "INFO: Executing SetupSSH.ps1"
 
 # Open the firewall
 Start-Process -wait netsh -ArgumentList "advfirewall firewall add rule name=SSH dir=in action=allow protocol=TCP localport=22"
-c:\cygwin\bin\bash -c 'export PATH=/usr/sbin:/usr/bin:$PATH;/usr/bin/ssh-host-config -y -w password -c \"binmode ntsec\";\
+c:\cygwin\bin\bash -c 'export PATH=/usr/sbin:/usr/bin:$PATH;/usr/bin/ssh-host-config -y -w {{ user `password` }} -c \"binmode ntsec\";\
 echo \"PasswordAuthentication no\" >> /etc/sshd_config;\
 mkdir -p ~/.ssh;\
 chmod 700 ~/.ssh;\
