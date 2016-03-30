28027bc17017e9c87c67b06fadf81999910ce3e9
John Howard <jhoward@microsoft.com>
2016-03-30 15:06:24 -0700

    More tweaks
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- packer/TP5/ConfigureScheduledTasksAfterSysprep.ps1
+++ packer/TP5/ConfigureScheduledTasksAfterSysprep.ps1
@@ -4,11 +4,11 @@
 
 Write-Host "INFO: Executing ConfigureScheduleTasksAfterSysprep.ps1"
 
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\scripts\SetupSSH.ps1"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\packer\SetupSSH.ps1"
 $trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
 Register-ScheduledTask -TaskName "SetupSSH" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
 
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\scripts\ActivateVM.ps1"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\packer\ActivateVM.ps1"
 $trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
 Register-ScheduledTask -TaskName "ActivateVM" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
 
@@ -16,19 +16,19 @@ $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command 
 $trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
 Register-ScheduledTask -TaskName "Kill-LongRunningDocker" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
 
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\scripts\SetupNSSM.ps1"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\packer\SetupNSSM.ps1"
 $trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
 Register-ScheduledTask -TaskName "SetupNSSM" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
 
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\scripts\ResetNetworking.ps1"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\packer\ResetNetworking.ps1"
 $trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
 Register-ScheduledTask -TaskName "ResetNetworking" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
 
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\scripts\UserTempToD.ps1"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\packer\UserTempToD.ps1"
 $trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
 Register-ScheduledTask -TaskName "UserTempToD" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
 
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\scripts\RebootOnce.ps1"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\packer\RebootOnce.ps1"
 $trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
 Register-ScheduledTask -TaskName "RebootOnce" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
 
