0e63f48fbcd44722fb9dc90bc5413c8968ca30b8
John Howard <jhoward@microsoft.com>
2016-03-30 21:31:42 -0700

    Merging stuff
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- packer/TP5/ConfigureScheduledTasksAfterSysprep.ps1
+++ packer/TP5/ConfigureScheduledTasksAfterSysprep.ps1
@@ -4,33 +4,12 @@
 
 Write-Host "INFO: Executing ConfigureScheduleTasksAfterSysprep.ps1"
 
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\packer\SetupSSH.ps1"
-$trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
-Register-ScheduledTask -TaskName "SetupSSH" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
-
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\packer\ActivateVM.ps1"
-$trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
-Register-ScheduledTask -TaskName "ActivateVM" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
-
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\scripts\Kill-LongRunningDocker.ps1"
 $trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
 Register-ScheduledTask -TaskName "Kill-LongRunningDocker" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
 
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\packer\SetupNSSM.ps1"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\scripts\PostSysprep.ps1"
 $trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
-Register-ScheduledTask -TaskName "SetupNSSM" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
-
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\packer\ResetNetworking.ps1"
-$trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
-Register-ScheduledTask -TaskName "ResetNetworking" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
-
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\packer\ConfigureTempToD.ps1"
-$trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
-Register-ScheduledTask -TaskName "ConfigureTempToD" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
-
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command c:\packer\RebootOnce.ps1"
-$trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
-Register-ScheduledTask -TaskName "RebootOnce" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
-
+Register-ScheduledTask -TaskName "PostSysprep" -Action $action -Trigger $trigger -User SYSTEM -RunLevel Highest
 
 Write-Host "INFO: ConfigureScheduledTasksAfterSysprep.ps1 completed"
---
+++ packer/TP5/PostSysprep.ps1
@@ -0,0 +1,97 @@
+#-----------------------
+# PostSysprep.ps1
+#-----------------------
+
+# Stop on error
+$ErrorActionPreference="stop"
+
+echo "PostSysprep.ps1 starting" > $env:SystemDrive\packer\postSysprep.txt
+
+#--------------------------------------------------------------------------------------------
+
+# Download and install Cygwin for SSH capability
+echo "PostSysprep.ps1 downloading cygwin..." > $env:SystemDrive\packer\postSysprep.txt
+mkdir $env:SystemDrive\cygwin -erroraction silentlycontinue 2>&1 | Out-Null
+$wc=New-Object net.webclient;$wc.Downloadfile("https://cygwin.com/setup-x86_64.exe","$env:SystemDrive\cygwin\cygwinsetup.exe")
+echo "PostSysprep.ps1 installing cygwin..." > $env:SystemDrive\packer\postSysprep.txt
+Start-Process $env:SystemDrive\cygwin\cygwinsetup.exe -ArgumentList "-q -R $env:SystemDrive\cygwin --packages openssh openssl -l $env:SystemDrive\cygwin\packages -s http://mirrors.sonic.net/cygwin/ 2>&1 | Out-Null" -Wait
+
+
+# Open the firewall
+echo "PostSysprep.ps1 opening firewall..." > $env:SystemDrive\packer\postSysprep.txt
+Start-Process -wait netsh -ArgumentList "advfirewall firewall add rule name=SSH dir=in action=allow protocol=TCP localport=22"
+
+# Configure cygwin
+echo "PostSysprep.ps1 configuring cygwin..." > $env:SystemDrive\packer\postSysprep.txt
+c:\cygwin\bin\bash -c 'export PATH=/usr/sbin:/usr/bin:$PATH;\
+mkdir -p ~/.ssh;\
+pass=$(< /cygdrive/c/packer/SSHpassword.txt);\
+if [ -z $pass]; then export pass="---ChAnGeMeJeNkInScIsErVeR---";fi;\
+/usr/bin/ssh-host-config -y -w $pass -c \"binmode ntsec\";\
+echo \"PasswordAuthentication no\" >> /etc/sshd_config;\
+chmod 700 ~/.ssh;\
+cp /cygdrive/c/packer/authorized_keys ~/.ssh/authorized_keys;\
+chmod 644 ~/.ssh/authorized_keys;\
+/usr/bin/cygrunsrv -S sshd' 2>&1 | out-null
+
+#--------------------------------------------------------------------------------------------
+
+# Activate the VM
+echo "PostSysprep.ps1 activating..." > $env:SystemDrive\packer\postSysprep.txt
+cscript $env:SystemDrive\windows\system32\slmgr.vbs /ipk 6XBNX-4JQGW-QX6QG-74P76-72V67
+cscript $env:SystemDrive\windows\system32\slmgr.vbs /ato
+
+#--------------------------------------------------------------------------------------------
+
+# Create directory for storing the nssm configuration
+mkdir $env:ProgramData\docker -ErrorAction SilentlyContinue 2>&1 | Out-Null
+
+# Install NSSM by extracting archive and placing in system32
+echo "PostSysprep.ps1 downloading NSSM configuration file..." > $env:SystemDrive\packer\postSysprep.txt
+$wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/nssmdocker.W2WCIServers.cmd","$env:ProgramData\docker\nssmdocker.cmd")
+echo "PostSysprep.ps1 downloading NSSM..." > $env:SystemDrive\packer\postSysprep.txt
+$wc=New-Object net.webclient;$wc.Downloadfile("https://nssm.cc/release/nssm-2.24.zip","$env:Temp\nssm.zip")
+echo "PostSysprep.ps1 extracting NSSM..." > $env:SystemDrive\packer\postSysprep.txt
+Expand-Archive -Path $env:Temp\nssm.zip -DestinationPath $env:Temp
+echo "PostSysprep.ps1 installing NSSM..." > $env:SystemDrive\packer\postSysprep.txt
+Copy-Item $env:Temp\nssm-2.24\win64\nssm.exe $env:SystemRoot\System32
+
+
+# Configure the docker NSSM service
+echo "PostSysprep.ps1 configuring NSSM..." > $env:SystemDrive\packer\postSysprep.txt
+Start-Process -Wait "nssm" -ArgumentList "install docker $($env:SystemRoot)\System32\cmd.exe /s /c $env:Programdata\docker\nssmdocker.cmd < nul"
+Start-Process -Wait "nssm" -ArgumentList "set docker DisplayName Docker Daemon"
+Start-Process -Wait "nssm" -ArgumentList "set docker Description Docker control daemon for CI testing"
+Start-Process -Wait "nssm" -ArgumentList "set docker AppStderr $env:Programdata\docker\nssmdaemon.log"
+Start-Process -Wait "nssm" -ArgumentList "set docker AppStdout $env:Programdata\docker\nssmdaemon.log"
+Start-Process -Wait "nssm" -ArgumentList "set docker AppStopMethodConsole 30000"
+
+#--------------------------------------------------------------------------------------------
+
+# This is a TP5 hack. #6925609. Should not be necessary in TP5 RTM.
+echo "PostSysprep.ps1 resetting networking (hack)..." > $env:SystemDrive\packer\postSysprep.txt
+netsh int ipv4 reset
+
+#--------------------------------------------------------------------------------------------
+
+echo "PostSysprep.ps1 configuring temp to D..." > $env:SystemDrive\packer\postSysprep.txt
+$env:Temp="d:\temp"
+$env:Tmp=$env:Temp
+[Environment]::SetEnvironmentVariable("TEMP", "$env:Temp", "Machine")
+[Environment]::SetEnvironmentVariable("TMP", "$env:Temp", "Machine")
+[Environment]::SetEnvironmentVariable("TEMP", "$env:Temp", "User")
+[Environment]::SetEnvironmentVariable("TMP", "$env:Temp", "User")
+
+#--------------------------------------------------------------------------------------------
+
+# Delete the scheduled task
+echo "PostSysprep.ps1 deleting scheduled task.." >> $env:SystemDrive\packer\postSysprep.txt
+$ConfirmPreference='none'
+Get-ScheduledTask 'PostSysprep' | Unregister-ScheduledTask
+echo "PostSysprep.ps1 complete" >> $env:SystemDrive\packer\postSysprep.txt
+
+#--------------------------------------------------------------------------------------------
+
+echo "PostSysprep.ps1 rebooting..." >> $env:SystemDrive\packer\postSysprep.txt
+shutdown /t 0 /r
+
