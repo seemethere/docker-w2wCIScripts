409f90d27a653f5f8d26e59a2f3728820c09b1f2
John Howard <jhoward@microsoft.com>
2016-04-01 15:03:26 -0700

    Log to .log not .txt and no activation
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- packer/TP5/ConfigurePostSysprep.ps1
+++ packer/TP5/ConfigurePostSysprep.ps1
@@ -6,15 +6,15 @@
 # so can schedule tasks as that user to do the post-sysprep configuration. This script itself though
 # is running as Local System.
 
-echo "$(date) ConfigurePostSysprep.ps1 starting..." >> $env:SystemDrive\packer\PostSysprep.txt
+echo "$(date) ConfigurePostSysprep.ps1 starting..." >> $env:SystemDrive\packer\PostSysprep.log
 
 # But... coming out of sysprep, we reboot twice, so we need to not do anything on the first reboot.
 if (-not (Test-Path c:\packer\ConfigurePostSysprep.GoneThroughOneReboot.txt)) {
-    echo "$(date) ConfigurePostSysprep.GoneThroughOneReboot.txt doesn't exist, so creating it and not doing anything..." >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) ConfigurePostSysprep.GoneThroughOneReboot.txt doesn't exist, so creating it and not doing anything..." >> $env:SystemDrive\packer\PostSysprep.log
     New-Item c:\packer\ConfigurePostSysprep.GoneThroughOneReboot.txt
     # Force a reboot if we don't get one in the next 10 minutes
-    sleep 600
-    shutdown /t 0 /r /f /c "10 minutes in ConfigurePostSysprep"
+    sleep 300
+    shutdown /t 0 /r /f /c "5 minutes in ConfigurePostSysprep"
     exit 0
 }
 
@@ -29,11 +29,11 @@ $trigger = New-ScheduledTaskTrigger -AtStartup -RandomDelay 00:01:00
 Register-ScheduledTask -TaskName "PostSysprep" -Action $action -Trigger $trigger -User jenkins -Password $pass -RunLevel Highest
 
 # Disable the scheduled task
-echo "$(date) ConfigurePostSysprep.ps1 disable scheduled task.." >> $env:SystemDrive\packer\PostSysprep.txt
+echo "$(date) ConfigurePostSysprep.ps1 disable scheduled task.." >> $env:SystemDrive\packer\PostSysprep.log
 $ConfirmPreference='none'
 Get-ScheduledTask 'ConfigurePostSysprep' | Disable-ScheduledTask
 
-echo "$(date) ConfigurePostSysprep.ps1 sleeping for 3 minutes before reboot..." >> $env:SystemDrive\packer\PostSysprep.txt
+echo "$(date) ConfigurePostSysprep.ps1 sleeping for 3 minutes before reboot..." >> $env:SystemDrive\packer\PostSysprep.log
 sleep 180
-echo "$(date) ConfigurePostSysprep.ps1 rebooting..." >> $env:SystemDrive\packer\PostSysprep.txt
+echo "$(date) ConfigurePostSysprep.ps1 rebooting..." >> $env:SystemDrive\packer\PostSysprep.log
 shutdown /t 0 /r /f /c "ConfigurePostSysprep"
--- packer/TP5/PostSysprep.ps1
+++ packer/TP5/PostSysprep.ps1
@@ -8,35 +8,35 @@ Write-Host "PostSysprep..."
 
 try {
 
-    echo "$(date) PostSysprep.ps1 starting" > $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 starting" > $env:SystemDrive\packer\PostSysprep.log
 
     #--------------------------------------------------------------------------------------------
 
     # Download and install Cygwin for SSH capability
-    echo "$(date) PostSysprep.ps1 downloading cygwin..." > $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 downloading cygwin..." > $env:SystemDrive\packer\PostSysprep.log
     mkdir $env:SystemDrive\cygwin -erroraction silentlycontinue 2>&1 | Out-Null
     $wc=New-Object net.webclient;$wc.Downloadfile("https://cygwin.com/setup-x86_64.exe","$env:SystemDrive\cygwin\cygwinsetup.exe")
-    echo "$(date) PostSysprep.ps1 installing cygwin..." >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 installing cygwin..." >> $env:SystemDrive\packer\PostSysprep.log
     Start-Process -wait $env:SystemDrive\cygwin\cygwinsetup.exe -ArgumentList "-q -R $env:SystemDrive\cygwin --packages openssh openssl -l $env:SystemDrive\cygwin\packages -s http://mirrors.sonic.net/cygwin/ 2>&1 | Out-Null"
     
     # Open the firewall
-    echo "$(date) PostSysprep.ps1 opening firewall..." >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 opening firewall..." >> $env:SystemDrive\packer\PostSysprep.log
     Start-Process -wait -NoNewWindow netsh -ArgumentList "advfirewall firewall add rule name=SSH dir=in action=allow protocol=TCP localport=22"
     
     # Configure cygwin. Run ConfigureSSH.sh twice. No idea why. Lost count of how many attempts to figure this out.
-    echo "$(date) PostSysprep.ps1 configuring cygwin..." >> $env:SystemDrive\packer\PostSysprep.txt
-    echo "$(whoami /all)" >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 configuring cygwin..." >> $env:SystemDrive\packer\PostSysprep.log
+    echo "$(whoami /all)" >> $env:SystemDrive\packer\PostSysprep.log
     Start-Process -wait taskkill -ArgumentList "/F /IM sshd.exe" -ErrorAction SilentlyContinue
-    Start-Process -wait -NoNewWindow c:\cygwin\bin\bash -ArgumentList "--login /cygdrive/c/packer/ConfigureSSH.sh >> /cygdrive/c/packer/PostSysprep.txt 2>&1"
+    Start-Process -wait -NoNewWindow c:\cygwin\bin\bash -ArgumentList "--login /cygdrive/c/packer/ConfigureSSH.sh >> /cygdrive/c/packer/PostSysprep.log 2>&1"
     Start-Process -wait taskkill -ArgumentList "/F /IM sshd.exe" -ErrorAction SilentlyContinue
-    Start-Process -wait -NoNewWindow c:\cygwin\bin\bash -ArgumentList "--login /cygdrive/c/packer/ConfigureSSH.sh >> /cygdrive/c/packer/PostSysprep.txt 2>&1"
+    Start-Process -wait -NoNewWindow c:\cygwin\bin\bash -ArgumentList "--login /cygdrive/c/packer/ConfigureSSH.sh >> /cygdrive/c/packer/PostSysprep.log 2>&1"
 
     #--------------------------------------------------------------------------------------------
     
     # Activate the VM
-    echo "$(date) PostSysprep.ps1 activating..." >> $env:SystemDrive\packer\PostSysprep.txt
-    cscript $env:SystemDrive\windows\system32\slmgr.vbs /ipk 6XBNX-4JQGW-QX6QG-74P76-72V67
-    cscript $env:SystemDrive\windows\system32\slmgr.vbs /ato
+    #echo "$(date) PostSysprep.ps1 activating..." >> $env:SystemDrive\packer\PostSysprep.log
+    #cscript $env:SystemDrive\windows\system32\slmgr.vbs /ipk 6XBNX-4JQGW-QX6QG-74P76-72V67
+    #cscript $env:SystemDrive\windows\system32\slmgr.vbs /ato
     
     #--------------------------------------------------------------------------------------------
     
@@ -44,18 +44,18 @@ try {
     mkdir $env:ProgramData\docker -ErrorAction SilentlyContinue 2>&1 | Out-Null
     
     # Install NSSM by extracting archive and placing in system32
-    echo "$(date) PostSysprep.ps1 downloading NSSM configuration file..." >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 downloading NSSM configuration file..." >> $env:SystemDrive\packer\PostSysprep.log
     $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/nssmdocker.W2WCIServers.cmd","$env:ProgramData\docker\nssmdocker.cmd")
-    echo "$(date) PostSysprep.ps1 downloading NSSM..." >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 downloading NSSM..." >> $env:SystemDrive\packer\PostSysprep.log
     $wc=New-Object net.webclient;$wc.Downloadfile("https://nssm.cc/release/nssm-2.24.zip","$env:Temp\nssm.zip")
-    echo "$(date) PostSysprep.ps1 extracting NSSM..." >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 extracting NSSM..." >> $env:SystemDrive\packer\PostSysprep.log
     Expand-Archive -Path $env:Temp\nssm.zip -DestinationPath $env:Temp
-    echo "$(date) PostSysprep.ps1 installing NSSM..." >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 installing NSSM..." >> $env:SystemDrive\packer\PostSysprep.log
     Copy-Item $env:Temp\nssm-2.24\win64\nssm.exe $env:SystemRoot\System32
     
     
     # Configure the docker NSSM service
-    echo "$(date) PostSysprep.ps1 configuring NSSM..." >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 configuring NSSM..." >> $env:SystemDrive\packer\PostSysprep.log
     Start-Process -Wait "nssm" -ArgumentList "install docker $($env:SystemRoot)\System32\cmd.exe /s /c $env:Programdata\docker\nssmdocker.cmd < nul"
     Start-Process -Wait "nssm" -ArgumentList "set docker DisplayName Docker Daemon"
     Start-Process -Wait "nssm" -ArgumentList "set docker Description Docker control daemon for CI testing"
@@ -66,12 +66,12 @@ try {
     #--------------------------------------------------------------------------------------------
     
     # This is a TP5 hack. #6925609. Should not be necessary in TP5 RTM.
-    echo "$(date) PostSysprep.ps1 resetting networking (hack)..." >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 resetting networking (hack)..." >> $env:SystemDrive\packer\PostSysprep.log
     netsh int ipv4 reset
     
     #--------------------------------------------------------------------------------------------
     
-    echo "$(date) PostSysprep.ps1 configuring temp to D..." >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 configuring temp to D..." >> $env:SystemDrive\packer\PostSysprep.log
     $env:Temp="d:\temp"
     $env:Tmp=$env:Temp
     [Environment]::SetEnvironmentVariable("TEMP", "$env:Temp", "Machine")
@@ -87,17 +87,17 @@ try {
     
 }
 Catch [Exception] {
-    echo "$(date) PostSysprep.ps1 complete with Error '$_'" >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 complete with Error '$_'" >> $env:SystemDrive\packer\PostSysprep.log
     exit 1
 }
 Finally {
 
     # Disable the scheduled task
-    echo "$(date) PostSysprep.ps1 deleting scheduled task.." >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 deleting scheduled task.." >> $env:SystemDrive\packer\PostSysprep.log
     $ConfirmPreference='none'
     Get-ScheduledTask 'PostSysprep' | Disable-ScheduledTask
 
-    echo "$(date) PostSysprep.ps1 rebooting..." >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 rebooting..." >> $env:SystemDrive\packer\PostSysprep.log
     shutdown /t 0 /r /f /c "PostSysprep"
     sleep 5
     shutdown /t 0 /r /f /c "PostSysprep"
@@ -109,5 +109,5 @@ Finally {
     shutdown /t 0 /r /f /c "PostSysprep"
     sleep 5
 
-    echo "$(date) PostSysprep.ps1 complete successfully at $(date)" >> $env:SystemDrive\packer\PostSysprep.txt
+    echo "$(date) PostSysprep.ps1 complete successfully at $(date)" >> $env:SystemDrive\packer\PostSysprep.log
 }    
