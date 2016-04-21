2c917f12474a3b83dfd5269c1b3271f508e499fb
John Howard <jhoward@microsoft.com>
2016-04-21 12:17:53 -0700

    un-rem runonciserver.cmd
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- runCI/RunOnCIServer.cmd
+++ runCI/RunOnCIServer.cmd
@@ -3,9 +3,9 @@ REM Wrapper script to perform a local run of CI assuming everything is already s
 REM This is set for the CI servers. The TP5+ servers should be pre-configured with these
 REM variables already - the C drive is the system drive, D is a fast SSD.
 
-REM set SOURCES_DRIVE=c
-REM set SOURCES_SUBDIR=gopath
-REM set TESTRUN_DRIVE=d
-REM set TESTRUN_SUBDIR=CI
+set SOURCES_DRIVE=c
+set SOURCES_SUBDIR=gopath
+set TESTRUN_DRIVE=d
+set TESTRUN_SUBDIR=CI
 
 sh ./executeCI.sh
