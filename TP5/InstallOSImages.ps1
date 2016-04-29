2ab03080eba94a364535c5d355ae67d445fc5e60
John Howard <jhoward@microsoft.com>
2016-04-29 15:46:26 -0700

    Stuff
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- TP5/InstallOSImages.ps1
+++ TP5/InstallOSImages.ps1
@@ -8,9 +8,9 @@ try {
     echo "$(date) InstallOSImages.ps1 starting" >> $env:SystemDrive\packer\configure.log
 
     echo "$(date) InstallOSImages.ps1 installing nanoserver image..." >> $env:SystemDrive\packer\configure.log
-    Install-ContainerOSImage c:\BaseImages\CBaseOs_rs1_release_svc_14300.1000.160324-1723_amd64fre_NanoServer_en-us.wim -Force
+    Install-ContainerOSImage c:\BaseImages\nanoserver.wim -Force
     echo "$(date) InstallOSImages.ps1 installing windowsservercore image..." >> $env:SystemDrive\packer\configure.log
-    Install-ContainerOSImage c:\BaseImages\CBaseOs_rs1_release_svc_14300.1000.160324-1723_amd64fre_ServerDatacenterCore_en-us.wim -Force
+    Install-ContainerOSImage c:\BaseImages\windowsservercore.wim -Force
 }
 Catch [Exception] {
     echo "$(date) InstallOSImages.ps1 complete with Error '$_'" >> $env:SystemDrive\packer\configure.log
