8c901f1505b645eb372047cdbeebc8595e205f6f
John Howard <jhoward@microsoft.com>
2016-04-28 21:54:37 -0700

    Open ssh firewall later
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- common/ConfigureSSH.ps1
+++ common/ConfigureSSH.ps1
@@ -14,10 +14,6 @@ try {
     echo "$(date) ConfigureSSH.ps1 Hack - installing windowsservercore image..." >> $env:SystemDrive\packer\configure.log
     Install-ContainerOSImage c:\BaseImages\CBaseOs_rs1_release_svc_14300.1000.160324-1723_amd64fre_ServerDatacenterCore_en-us.wim -Force
 
-    # Open the firewall
-    echo "$(date) ConfigureSSH.ps1 opening firewall for SSH..." >> $env:SystemDrive\packer\configure.log
-    Start-Process -wait -NoNewWindow netsh -ArgumentList "advfirewall firewall add rule name=SSH dir=in action=allow protocol=TCP localport=22"
-
     # Configure cygwin ssh daemon
     Start-Process -wait taskkill -ArgumentList "/F /IM sshd.exe" -ErrorAction SilentlyContinue
     Start-Process -wait -WorkingDirectory c:\packer -NoNewWindow c:\cygwin\bin\bash -ArgumentList "--login /cygdrive/c/packer/ConfigureSSH.sh >> /cygdrive/c/packer/configure.log 2>&1"
@@ -38,6 +34,11 @@ Finally {
     Remove-Item "C:\Users\jenkins\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\ConfigureSSH.lnk" -Force -ErrorAction SilentlyContinue
     Remove-Item c:\packer\password.txt -Force -ErrorAction SilentlyContinue
     Remove-Item c:\packer\ConfigureSSH.log -Force -ErrorAction SilentlyContinue
+
+    # Open the firewall
+    echo "$(date) ConfigureSSH.ps1 opening firewall for SSH..." >> $env:SystemDrive\packer\configure.log
+    Start-Process -wait -NoNewWindow netsh -ArgumentList "advfirewall firewall add rule name=SSH dir=in action=allow protocol=TCP localport=22"
+
     echo "$(date) ConfigureSSH.ps1 is rebooting" >> $env:SystemDrive\packer\configure.log
     shutdown /t 0 /r /f /c "ConfigureSSH.ps1"
 } 
