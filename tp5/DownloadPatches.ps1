210e749dbe8719c1823fbed8e5053c486a37bcbd
John Howard <jhoward@microsoft.com>
2016-05-17 09:58:00 -0700

    Rename complete hopefully
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- tp5/DownloadPatches.ps1
+++ tp5/DownloadPatches.ps1
@@ -23,26 +23,26 @@ try {
 
     # ZDP varieties
     echo "$(date) DownloadPatches.ps1 Downloading ZDP..." >> $env:SystemDrive\packer\configure.log
-    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/TP5/ZDP/5B/Windows10.0-KB3158987-x64.msu","c:\ZDP\5B\Windows10.0-KB3158987-x64.msu")
-    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/TP5/ZDP/4D/Windows10.0-KB3157663-x64.msu","c:\ZDP\4D\Windows10.0-KB3157663-x64.msu")
-    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/TP5/ZDP/Pre4D/1.000/Windows10.0-KB3155191-x64.msu","c:\ZDP\Pre4D\1.000\Windows10.0-KB3155191-x64.msu")
-    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/TP5/ZDP/Pre4D/1.003/Windows10.0-KB3155191-x64.msu","c:\ZDP\Pre4D\1.003\Windows10.0-KB3155191-x64.msu")
+    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/tp5/ZDP/5B/Windows10.0-KB3158987-x64.msu","c:\ZDP\5B\Windows10.0-KB3158987-x64.msu")
+    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/tp5/ZDP/4D/Windows10.0-KB3157663-x64.msu","c:\ZDP\4D\Windows10.0-KB3157663-x64.msu")
+    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/tp5/ZDP/Pre4D/1.000/Windows10.0-KB3155191-x64.msu","c:\ZDP\Pre4D\1.000\Windows10.0-KB3155191-x64.msu")
+    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/tp5/ZDP/Pre4D/1.003/Windows10.0-KB3155191-x64.msu","c:\ZDP\Pre4D\1.003\Windows10.0-KB3155191-x64.msu")
 
     # Privates - utilities
     echo "$(date) DownloadPatches.ps1 Downloading Utilities..." >> $env:SystemDrive\packer\configure.log
-    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/TP5/Privates/certutil.exe","c:\privates\certutil.exe")
-    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/TP5/Privates/testroot-sha2.cer","c:\privates\testroot-sha2.cer")
-    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/TP5/Privates/sfpcopy.exe","c:\privates\sfpcopy.exe")
+    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/tp5/Privates/certutil.exe","c:\privates\certutil.exe")
+    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/tp5/Privates/testroot-sha2.cer","c:\privates\testroot-sha2.cer")
+    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/tp5/Privates/sfpcopy.exe","c:\privates\sfpcopy.exe")
 
     # Privates - binaries
     echo "$(date) DownloadPatches.ps1 Downloading Privates..." >> $env:SystemDrive\packer\configure.log
-    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/TP5/Privates/HostNetSvc.dll","c:\privates\HostNetSvc.dll")
-    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/TP5/Privates/NetMgmtIF.dll","c:\privates\NetMgmtIF.dll")
-    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/TP5/Privates/NetSetupApi.dll","c:\privates\NetSetupApi.dll")
-    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/TP5/Privates/NetSetupEngine.dll","c:\privates\NetSetupEngine.dll")
-    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/TP5/Privates/NetSetupSvc.dll","c:\privates\NetSetupSvc.dll")
-    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/TP5/Privates/ntkrnlmp.exe","c:\privates\ntkrnlmp.exe")
-    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/TP5/Privates/wcifs.sys","c:\privates\wcifs.sys")
+    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/tp5/Privates/HostNetSvc.dll","c:\privates\HostNetSvc.dll")
+    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/tp5/Privates/NetMgmtIF.dll","c:\privates\NetMgmtIF.dll")
+    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/tp5/Privates/NetSetupApi.dll","c:\privates\NetSetupApi.dll")
+    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/tp5/Privates/NetSetupEngine.dll","c:\privates\NetSetupEngine.dll")
+    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/tp5/Privates/NetSetupSvc.dll","c:\privates\NetSetupSvc.dll")
+    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/tp5/Privates/ntkrnlmp.exe","c:\privates\ntkrnlmp.exe")
+    $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/tp5/Privates/wcifs.sys","c:\privates\wcifs.sys")
 
     }
 Catch [Exception] {
