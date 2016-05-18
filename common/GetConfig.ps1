d8004a5b93b5b381f69f55fccab06332c467ff1f
John Howard <jhoward@microsoft.com>
2016-05-18 12:28:32 -0700

    Working on matching
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- common/GetConfig.ps1
+++ common/GetConfig.ps1
@@ -5,12 +5,34 @@
 try {
 
     if ([string]::IsNullOrWhiteSpace($Branch)) {
+        $Branch=""
         
         # Get config.txt
-        echo "$(date) GetConfig.ps1 Downloading config.txt..." >> $env:SystemDrive\packer\configure.log
+        echo "$(date) Bootstrap.ps1 Downloading config.txt..." >> $env:SystemDrive\packer\configure.log
         $wc=New-Object net.webclient;$wc.Downloadfile("https://raw.githubusercontent.com/jhowardmsft/docker-w2wCIScripts/master/config/config.txt","$env:SystemDrive\packer\config.txt")
 
-
+        $hostname=$env:COMPUTERNAME.ToLower()
+        echo "$(date) Bootstrap.ps1 Matching $hostname for a branch type..." >> $env:SystemDrive\packer\configure.log
+        
+        foreach ($line in Get-Content $env:SystemDrive\packer\config.txt) {
+            $line=$line.Trim()
+            if (($line[0] -eq "#") -or ($line -eq "")) {
+                continue
+            }
+            $elements=$line.Split(",")
+            if ($elements.Length -ne 2) {
+                continue
+            }
+            if (($elements[0].Length -eq 0) -or ($elements[1].Length -eq 0)) {
+                continue
+            }
+            if ($hostname -match $elements[0]) {
+                $Branch=$elements[1]
+                Write-Host $hostname matches $elements[1]
+                break
+            }
+        }
+        if ($Branch.Length -eq 0) { Throw "Branch not supplied and $hostname regex match not found in configuration" }
      }
 
     # Store the branch
@@ -21,4 +43,5 @@ Catch [Exception] {
 }
 Finally {
     Write-Host "$(date) GetConfig.ps1 completed..." 
+	type c:\packer\configure.log
 }  
