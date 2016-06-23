592fbc6410c2c2eff9f59254e78fe00d6c33f354
John Howard <jhoward@microsoft.com>
2016-06-23 12:26:02 -0700

    Moving to tar
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- common/Phase3.ps1
+++ common/Phase3.ps1
@@ -20,27 +20,25 @@ try {
     echo "$(date) Phase3.ps1 Installing OS images..." >> $env:SystemDrive\packer\configure.log    
     . $("$env:SystemDrive\packer\InstallOSImages.ps1")
 
-    if ($env:LOCAL_CI_INSTALL -ne 1) {
-        #--------------------------------------------------------------------------------------------
-        # Initiate Phase4. This runs as the Jenkins user interactively. This is particularly on account
-        # of configuring cygwin SSH not working as local system, RunOnce regkey also fails. And so does
-        # a scheduled task that isn't interactive. All due to cygwin oddities.
-        echo "$(date) Phase3.ps1 Initiating phase4..." >> $env:SystemDrive\packer\configure.log    
-        $pass = Get-Content c:\packer\password.txt -raw
-        REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WinLogon" /v AutoAdminLogon /t REG_DWORD /d 1 /f | Out-Null
-        REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WinLogon" /v DefaultUserName /t REG_SZ /d jenkins /f | Out-Null
-        REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WinLogon" /v DefaultPassword /t REG_SZ /d $pass /f | Out-Null
+    #--------------------------------------------------------------------------------------------
+    # Initiate Phase4. This runs as the Jenkins user interactively. This is particularly on account
+    # of configuring cygwin SSH not working as local system, RunOnce regkey also fails. And so does
+    # a scheduled task that isn't interactive. All due to cygwin oddities.
+    echo "$(date) Phase3.ps1 Initiating phase4..." >> $env:SystemDrive\packer\configure.log    
+    $pass = Get-Content c:\packer\password.txt -raw
+    REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WinLogon" /v AutoAdminLogon /t REG_DWORD /d 1 /f | Out-Null
+    REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WinLogon" /v DefaultUserName /t REG_SZ /d jenkins /f | Out-Null
+    REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\WinLogon" /v DefaultPassword /t REG_SZ /d $pass /f | Out-Null
     
-        # Create the shortcut (and the containing directory as we haven't logged on interactively yet)
-        New-Item "C:\Users\jenkins\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Type Directory -ErrorAction SilentlyContinue    
-        $TargetFile = "powershell"
-        $ShortcutFile = "C:\Users\jenkins\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Phase4.lnk"
-        $WScriptShell = New-Object -ComObject WScript.Shell
-        $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-        $Shortcut.Arguments ="-command c:\packer\Phase4.ps1"
-        $Shortcut.TargetPath = $TargetFile
-        $Shortcut.Save()
-    }
+    # Create the shortcut (and the containing directory as we haven't logged on interactively yet)
+    New-Item "C:\Users\jenkins\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Type Directory -ErrorAction SilentlyContinue    
+    $TargetFile = "powershell"
+    $ShortcutFile = "C:\Users\jenkins\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Phase4.lnk"
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+    $Shortcut.Arguments ="-command c:\packer\Phase4.ps1"
+    $Shortcut.TargetPath = $TargetFile
+    $Shortcut.Save()
 }
 Catch [Exception] {
     echo "$(date) Phase3.ps1 complete with Error '$_'" >> $env:SystemDrive\packer\configure.log
