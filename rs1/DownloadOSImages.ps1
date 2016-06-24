4bb89b9925d5b30ed7de3dd427d2ab8834c2ea9f
John Howard <jhoward@microsoft.com>
2016-06-24 15:20:35 -0700

    Minor tweaks
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- rs1/DownloadOSImages.ps1
+++ rs1/DownloadOSImages.ps1
@@ -10,19 +10,12 @@ try {
     echo "$(date) DownloadOSImages.ps1 starting." >> $env:SystemDrive\packer\configure.log
     New-Item "C:\BaseImages" -ItemType Directory -ErrorAction SilentlyContinue
 
-    if (Test-Path c:\baseimages\$type.tar) {
-        echo "$(date) DownloadOSImages.ps1 c:\baseimages\$type.tar exists - nothing to do" >> $env:SystemDrive\packer\configure.log    
-        return
-    }
-
     # Copy from internal share
     $bl=(Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion"  -Name BuildLabEx).BuildLabEx
     $a=$bl.ToString().Split(".")
     $Branch=$a[3]
     $Build=$a[0]+"."+$a[1]+"."+$a[4]
     $Location="\\winbuilds\release\$Branch\$Build\amd64fre\ContainerBaseOsPkgs"
-    $type="windowsservercore"
-    $BuildName="serverdatacentercore"  # Internal build name for windowsservercore
 
     if ($(Test-Path $Location) -eq $False) {
         Throw "$Location inaccessible. If not on Microsoft corpnet, copy $type.tar manually to c:\baseimages"
@@ -33,10 +26,19 @@ try {
     Register-PackageSource -Name HyperVDev -Provider PowerShellGet -Location \\redmond\1Windows\TestContent\CORE\Base\HYP\HAT\packages -Trusted -Force | Out-Null
     Install-Module -Name Containers.Layers -Repository HyperVDev | Out-Null
     Import-Module Containers.Layers | Out-Null
-            
+
+    $type="windowsservercore"
+    $BuildName="serverdatacentercore"  # Internal build name for windowsservercore
     $SourceTar="$Location\cbaseospkg_"+$BuildName+"_en-us\CBaseOS_"+$Branch+"_"+$Build+"_amd64fre_"+$BuildName+"_en-us.tar.gz"
     echo "$(date) DownloadOSImages.ps1 Converting $SourceTar. This may take a few minutes...." >> $env:SystemDrive\packer\configure.log    
     Export-ContainerLayer -SourceFilePath $SourceTar -DestinationFilePath c:\BaseImages\$type.tar
+
+    $type="nanoserver"
+    $BuildName="nanoserver"
+    $SourceTar="$Location\cbaseospkg_"+$BuildName+"_en-us\CBaseOS_"+$Branch+"_"+$Build+"_amd64fre_"+$BuildName+"_en-us.tar.gz"
+    echo "$(date) DownloadOSImages.ps1 Converting $SourceTar. This may take a few minutes...." >> $env:SystemDrive\packer\configure.log    
+    Export-ContainerLayer -SourceFilePath $SourceTar -DestinationFilePath c:\BaseImages\$type.tar
+
 }
 Catch [Exception] {
     echo "$(date) DownloadOSImages.ps1 complete with Error '$_'" >> $env:SystemDrive\packer\configure.log
