42cf19b94896e919230eaf6d15fb0cf25c3a3727
John Howard <jhoward@microsoft.com>
2016-07-01 15:51:23 -0700

    Deploy updated for no packer
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- Deploy-JenkinsVM/Deploy-JenkinsVM.ps1
+++ Deploy-JenkinsVM/Deploy-JenkinsVM.ps1
@@ -2,11 +2,10 @@
 param(
     [Parameter(Mandatory=$false)][string]$vmName,
     [Parameter(Mandatory=$false)][switch]$Force=$False,
-    [Parameter(Mandatory=$false)][string]$storageAccount, # Storage account for the image and where VM is created eg tp5 or winrs1
     [Parameter(Mandatory=$false)][string]$size="D3", # Size of the VM
-    [Parameter(Mandatory=$false)][string]$ImagePrefix, # Image matching string. Must start with this
+    [Parameter(Mandatory=$false)][string]$ImageVersion, # Image version
+    [Parameter(Mandatory=$false)][string]$BranchType, # eg rs1
     [Parameter(Mandatory=$false)][string]$Password=$env:JENKINS_PASSWORD_W2W 
-    ###[Parameter(Mandatory=$false)][string]$AzureSubscriptionID=$env:AZURE_SUBSCRIPTION_ID 
 )
 
 $vnetSiteName = 'Jenkins'             # Network to connect to
@@ -14,15 +13,18 @@ $defaultLocation = 'Central US'       # Hopefully obvious
 $size = "Standard_"+"$size"+"_v2"     # Size of the VM
 
 $adminUsername = 'jenkins'
-###$defaultPublishSettings = '$env:HOMEPATH/.azure/engine-team@docker.com.publishsettings'
 
 if ([string]::IsNullOrWhiteSpace($Password)) {
      Throw "Password for the user 'jenkins' must be supplied, or provided in $env:JENKINS_PASSWORD_W2W"
 }
 
-##if ([string]::IsNullOrWhiteSpace($AzureSubscriptionID)) {
-##     Throw "The Azure Subscription ID must be supplied, or provided in $env:AZURE_SUBSCRIPTION_ID"
-##}
+if ([string]::IsNullOrWhiteSpace($ImageVersion)) {
+     Throw "ImageVersion must be supplied. It's the nnnnn bit in AzureRS1vnnnnn.vhd for example"
+}
+
+if ([string]::IsNullOrWhiteSpace($BranchType)) {
+     Throw "BranchType must be supplied. It's the rs1 bit in AzureRS1vnnnnn.vhd for example"
+}
 
 $ErrorActionPreference = 'Stop'
 
@@ -39,16 +41,6 @@ function ask {
 
 try {
     if ([string]::IsNullOrWhiteSpace($vmName)) { Throw("vmName parameter must be supplied") }
-    if ([string]::IsNullOrWhiteSpace($storageAccount)) { Throw("storageAccount parameter must be supplied - winrs1 or tp5") }
-    if ([string]::IsNullOrWhiteSpace($imagePrefix)) { Throw("imagePrefix parameter must be supplied - jenkins-rs1 or jenkins-tp5") }
-
-###    if (Test-Path $defaultPublishSettings) {
-###        Write-Host "INFO: Importing Publish Settings File"
-###        Import-AzurePublishSettingsFile $defaultPublishSettings
-###    }
-
-#    Write-Host "INFO: Configuring the Azure Subscription"
-#    Set-AzureSubscription -SubscriptionId $AzureSubscriptionID -CurrentStorageAccountName $storageAccount
 
     Write-Host "INFO: Checking if VM exists"
     $vm = Get-AzureVM -ServiceName $vmName -Name $vmName -ErrorAction SilentlyContinue
@@ -69,17 +61,9 @@ try {
         Write-Host "INFO: Service exists."
     }
 
-    Write-Host "INFO: Determining the latest '$imagePrefix' image..."
-    $sourceImages = Get-AzureVMImage | Where-Object { $_.ImageName -like "$imagePrefix*" } | Sort-Object -Descending CreatedTime
-    if ($sourceImages -eq $null) {
-        Throw "No source images were found"
-    }
-    $sourceImageName = $sourceImages[0].ImageName
-    Write-Host "INFO: Latest is $sourceImageName"
-    ask("Right image?")
-
     Write-Host "INFO: Creating the configuration object..."
-    $vmc = New-AzureVMConfig -ImageName $sourceImageName -InstanceSize $size -Name $vmName -DiskLabel $vmName |
+    $ImageName="azure"+$BranchType+"v"+$ImageVersion
+    $vmc = New-AzureVMConfig -ImageName $ImageName -InstanceSize $size -Name $vmName -DiskLabel $vmName |
             Add-AzureEndpoint -Name 'SSH' -LocalPort 22 -PublicPort 22 -Protocol tcp |
             Set-AzureSubnet 'Subnet-1'
 
