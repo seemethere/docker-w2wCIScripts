d09ae905cc66673e814599cddb404af841ab45ba
John Howard <jhoward@microsoft.com>
2016-07-27 13:40:18 -0700

    Comment out autoadminlogon removal then!
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- runCI/testxenon.ps1
+++ runCI/testxenon.ps1
@@ -6,7 +6,8 @@ e:\docker\ci\w2w\runCI\Invoke-DockerCI.ps1 `
 -WindowsBaseImage nanoserver `
 -CIScriptLocation E:\docker\ci\w2w\runci\executeCI.ps1 `
 -GitRemote https://github.com/microsoft/docker `
--GitCheckout "jjh/containercounts" 
+-GitCheckout "jjh/containercounts" `
+-DUTDebugMode 
 
 #-skipimagebuild `
 #-skipbinarybuild `
