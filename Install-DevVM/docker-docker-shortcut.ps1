74065d6c7ae3a4478e443d6daa232ca66493bc24
John Howard (VM) <jhoward@ntdev.microsoft.com>
2017-03-09 15:47:55 -0800

    Update shortcut creation for dev VM
    Signed-off-by: John Howard (VM) <jhoward@ntdev.microsoft.com>

--- Install-DevVM/docker-docker-shortcut.ps1
+++ Install-DevVM/docker-docker-shortcut.ps1
@@ -1,7 +1,7 @@
-    $TargetFile = "e:\docker\utils\environ.cmd"
+    $TargetFile = "powershell"
     $ShortcutFile = "C:\Users\$env:Username\Desktop\docker-docker.lnk"
     $WScriptShell = New-Object -ComObject WScript.Shell
     $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-    $Shortcut.Arguments ="docker docker"
+    $Shortcut.Arguments ="-noexit e:\docker\utils\environ.ps1 docker docker"
     $Shortcut.TargetPath = $TargetFile
     $Shortcut.Save()
