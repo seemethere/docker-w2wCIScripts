a70ace649a14987b24b94a322149513c9eda3e9a
John Howard (VM) <jhoward@ntdev.microsoft.com>
2017-03-21 10:37:54 -0700

    Notepad++ CIA hack update; JRE update
    Signed-off-by: John Howard (VM) <jhoward@ntdev.microsoft.com>

--- common/InstallMostThings.ps1
+++ common/InstallMostThings.ps1
@@ -8,8 +8,11 @@
 echo "$(date) InstallMostThings.ps1 starting" >> $env:SystemDrive\packer\configure.log
 
 $GIT_LOCATION="https://github.com/git-for-windows/git/releases/download/v2.11.0.windows.1/PortableGit-2.11.0-64-bit.7z.exe"
-$JDK_LOCATION="http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-windows-x64.exe"  # 12/2/2016
-$NPP_LOCATION="https://notepad-plus-plus.org/repository/7.x/7.2.2/npp.7.2.2.Installer.x64.exe" # 12/2/2016
+#$JDK_LOCATION="http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-windows-x64.exe"  # 12/2/2016
+$JDK_LOCATION="http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jre-8u121-windows-x64.exe" #3/21/2017
+
+
+$NPP_LOCATION="https://notepad-plus-plus.org/repository/7.x/7.3.3/npp.7.3.3.Installer.x64.exe" # 3/21/2017 - CIA hack...
 $DOCKER_LOCATION="https://master.dockerproject.org/windows/amd64"
 $DELVE_LOCATION="github.com/derekparker/delve/cmd/dlv"
 
